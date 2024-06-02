c9ef279a30cca81929b52280c8f15a29807fe9df
newweww <nattanan2545@hotmai.com>
2024-06-02 17:53:42 +0700

    finish transition / finish mobile nav

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const projects = () => {
   return (
     <div>projects</div>
---
+++ components/MobileNav.jsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+import { CiMenuFries } from "react-icons/ci"
+
+const links = [
+    {
+        name: 'home',
+        path: '/',
+    },
+    {
+        name: 'resume',
+        path: '/resume',
+    },
+    {
+        name: 'projects',
+        path: '/projects',
+    },
+    {
+        name: 'contact',
+        path: '/contact',
+    },
+]
+
+const MobileNav = () => {
+    const pathname = usePathname()
+    return (
+        <Sheet>
+            <SheetTrigger className="flex justify-center items-center">
+                <CiMenuFries className="text-[32px] text-accent" />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+                <div className="mt-32 mb-40 text-center text-2xl">
+                    <Link href="/">
+                        <h1 className="text-4xl font-semibold">New<span className="text-accent">.</span></h1>
+                    </Link>
+                </div>
+
+                <div className="flex flex-col justify-center items-center">
+                    {links.map((link, index) => {
+                        return (
+                            <Link key={index} href={link.path} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} text-xl capitalize hover:text-accent transition-all `}>
+                                {link.name}
+                            </Link>
+                        )
+                    })}
+                </div>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileNav
