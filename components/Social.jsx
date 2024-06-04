930c1177860ee70f6872ee1f85573ffbc29e951c
newweww <nattanan2545@hotmai.com>
2024-06-04 19:39:28 +0700

    finish homepage and fix scaling

---
+++ components/Social.jsx
@@ -0,0 +1,26 @@
+"use client"
+import Link from "next/link"
+
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa"
+
+const socials = [
+    { icon: <FaGithub />, path: "https://github.com" },
+    { icon: <FaLinkedinIn />, path: "https://linkedin.com" },
+    { icon: <FaInstagram />, path: "https://instagram.com" }
+]
+
+const Social = ({ containerStyles, iconStyles }) => {
+    return (
+        <div className={containerStyles}>
+            {socials.map((item, index) => {
+                return (
+                    <Link key={index} href={item.path} className={iconStyles}>
+                        {item.icon}
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Social
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-accent text-primary hover:bg-accent-hover",
         primary: "bg-primay text-white",
-        outline: "border bordre-acent bg-transparent text-accent hover:bg-accent hover:text-primary"
+        outline: "border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary"
       },
       size: {
         default: "h-[44px] px-6",
