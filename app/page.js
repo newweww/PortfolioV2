7aba206e8635a8c8d8b25ab080a55c6fc01e8e41
newweww <nattanan2545@hotmai.com>
2024-06-04 13:49:15 +0700

    start home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { FiDownload } from "react-icons/fi"
 
-export default function Home() {
+const Home = () => {
   return (
-    <main>Homepage</main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full"> 
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span>Fullstack Developer</span>
+            <h1 className="h1">Hello I,m <br /><span>New</span></h1>
+          </div>
+          <div>
+            img
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
