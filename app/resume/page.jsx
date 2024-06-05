d630f26c0b12034374ff6a8528e9f31d80231147
newweww <nattanan2545@hotmai.com>
2024-06-06 01:25:29 +0700

    start progressing with resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -75,13 +75,36 @@ const skills = {
   ]
 }
 
-import { Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { delay, motion } from "framer-motion"
 
 const Resume = () => {
-    return (
-      <div>Resume</div>
-    )
-  }
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="about" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="about">About</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+          </TabsList>
+
+          <div>
+            contextn
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  )
+}
 
 export default Resume
